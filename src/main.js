541b9b6b62bb6c3ec3ebde9950880a36ddb9cf8c
ctf <18720781993@139.com>
2022-11-22 16:29:49 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css'
+import 'src/styles/iconfont.css'
+// 动态设置基准值
+import 'amfe-flexible'
+import Vant from 'vant'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
---
+++ src/utils/request.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import store from '@/store'
+import router from '@/router'
+const request = axios.create({
+  baseURL: 'http://toutiao.itheima.net'
+})
+// 请求拦截器统一注入token
+request.interceptors.request.use(config => {
+  const { users } = store.state
+  if (users && users.token) {
+    config.headers.Authorization = `Bear ${users.token}`
+  }
+  return config
+}, err => Promise.reject(err)
+)
+// 请求拦截器脱掉一层data
+request.interceptors.response.use(res => res.data, async err => {
+  if (err.response && err.response.status === 401) {
+    const { users } = store.state
+    if (!users && !users.refresh_token) return router.push('/login')
+    // 如果有refresh_token，则请求获取新的 token
+    try {
+      const res = await axios({
+        method: 'PUT',
+        url: '/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${users.refresh_token}`
+        }
+      })
+
+      // 如果获取成功，则把新的 token 更新到容器中
+      console.log('刷新 token  成功', res)
+      store.commit('setUser', {
+        token: res.data.data.token, // 最新获取的可用 token
+        refresh_token: users.refresh_token // 还是原来的 refresh_token
+      })
+
+      // 把之前失败的用户请求继续发出去
+      // config 是一个对象，其中包含本次失败请求相关的那些配置信息，例如 url、method 都有
+      // return 把 request 的请求结果继续返回给发请求的具体位置
+      return request(err.config)
+    } catch (err2) {
+      // 如果获取失败，直接跳转 登录页
+      console.log('请求刷线 token 失败', err2)
+      this.$router.push('/login')
+    }
+  }
+  return Promise.reject(err)
+})
